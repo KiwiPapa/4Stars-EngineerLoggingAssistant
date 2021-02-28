388471b07315c1fb04d304a6206617051304d35f
KiwiPapa <bmwmsn123@icloud.com>
2021-02-28 12:00:08 +0800

    规范化功能集成

--- 规范化脚本-有待进一步测试.py
+++ 规范化脚本-有待进一步测试.py
@@ -62,7 +62,9 @@ if int(col_num) > 10:
     # 获取行数
     info = load_ws.used_range
     last_row = info.last_cell.row
-    alpha_dict = {1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'E', 6: 'F', 7: 'G', 8: 'H', 9: 'I', 10: 'J', 11: 'K', 12: 'L', 13: 'M', 14: 'N', 15: 'O', 16: 'P', 17: 'Q', 18: 'R', 19: 'S', 20: 'T', 21: 'U', 22: 'V', 23: 'W', 24: 'X', 25: 'Y', 26: 'Z'}
+    alpha_dict = {1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'E', 6: 'F', 7: 'G', 8: 'H', 9: 'I', 10: 'J', 11: 'K', 12: 'L',
+                  13: 'M', 14: 'N', 15: 'O', 16: 'P', 17: 'Q', 18: 'R', 19: 'S', 20: 'T', 21: 'U', 22: 'V', 23: 'W',
+                  24: 'X', 25: 'Y', 26: 'Z'}
     last_column_num = info.last_cell.column
     last_column = alpha_dict[last_column_num]
     print(last_row, ' ', last_column)
